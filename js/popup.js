ad1c6fa32c9c0cbfe8911667ff1a4932664a58cf
Miksyu <vozmilova05@gmail.com>
2021-03-02 22:10:33 +0300

    Create popup.js

---
+++ js/popup.js
@@ -0,0 +1,17 @@
+import {createAds} from './create-ads.js';
+
+const similarAdsTemplate = document.querySelector('#card');
+
+//const popup = document.querySelector('.popup');
+
+const similarAds = createAds;
+similarAds.forEach((offer) => {
+  const adElement = similarAdsTemplate.cloneNode(true);
+  const card = adElement.querySelector('.popup');
+  const titleTemplate = adElement.querySelector('.popup__type'); debugger
+  titleTemplate.textContent = offer.offer.title;
+  adElement.querySelector('.popup__text--address').textContent = offer.offer.address;
+  adElement.querySelector('.popup__text--price').textContent = offer.offer.price;
+  debugger
+});
+
