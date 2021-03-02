a833d2a45f6fd38e317f46e2b13cd8e896775a37
Miksyu <vozmilova05@gmail.com>
2021-03-02 23:09:47 +0300

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,16 @@
-import {createAds} from './create-ads.js';
+//import {createAds} from './create-ads.js';
 
-const similarAdsTemplate = document.querySelector('#card');
+/*const similarAdsTemplate = document.querySelector('#card');
 
-//const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 
 const similarAds = createAds;
 similarAds.forEach((offer) => {
   const adElement = similarAdsTemplate.cloneNode(true);
-  //const card = adElement.querySelector('.popup');
-  //const titleTemplate = adElement.querySelector('.popup__type');
-  //titleTemplate.textContent = offer.offer.title;
+  const card = adElement.querySelector('.popup');
+  const titleTemplate = adElement.querySelector('.popup__type');
+  titleTemplate.textContent = offer.offer.title;
   adElement.querySelector('.popup__text--address').textContent = offer.offer.address;
   adElement.querySelector('.popup__text--price').textContent = offer.offer.price;
-});
+});*/
 
